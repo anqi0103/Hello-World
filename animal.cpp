e141b9c73467fb04dcd9ce93db74932c27f10369
anqi0103 <36179095+anqi0103@users.noreply.github.com>
2019-01-13 21:33:55 -0800

    Test File

---
+++ animal.cpp
@@ -0,0 +1,22 @@
+
+/******************************************************************
+ ** Author: Anqi Lu
+ ** Date: June 26, 2018
+ ** Description: Ask the user for their favorite animal and then prints
+ **              out that that is their favorite animal.
+ ******************************************************************/
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string faveAnimal;
+	std::cout << "Please enter your favorite animal." << std::endl;
+	std::cin >> faveAnimal;
+	std::cout << "Your favorite animal is the " << faveAnimal;
+	std::cout << "." <<std::endl;
+
+	return 0;
+}
+
+
